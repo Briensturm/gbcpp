953d2d62a2a78f7b66bc5efeecfd4a4c460a73da
Briensturm <briensturm@googlemail.com>
2020-12-30 15:24:35 +0100

    Implement bit instructions

---
+++ src/cpu/instructions/bit/bitn_hl_.hpp
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "instruction.hpp"
+
+class BITN_HL_ : public Instruction
+{
+    public:
+        int GetInstructionLength() { return 2; }
+
+        void ExecuteCycle(CpuStatePtr& cpuState, RamPtr& mainMemory)
+        {          
+            switch (_remainingCycles)
+            {
+                case 2:
+                    _currentData = mainMemory->ReadByte(cpuState->Registers->HL);
+                    break;
+                case 1:
+                    auto bitIndex = (_opcode & 0x38) >> 3;
+                    cpuState->Registers->ZeroFlag = ((byte)(_currentData >> bitIndex) & 0x01) == 0;
+
+                    cpuState->Registers->HalfCarryFlag = true;
+                    cpuState->Registers->SubtractionFlag = false;
+                    break;
+            }
+
+            Instruction::ExecuteCycle(cpuState, mainMemory);
+        }
+
+    private:
+        byte _currentData;
+};
---
+++ src/cpu/instructions/bit/bitnr8.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "instruction.hpp"
+
+class BITNR8 : public Instruction
+{
+    public:
+        int GetInstructionLength() { return 1; }
+
+        void ExecuteCycle(CpuStatePtr& cpuState, RamPtr& mainMemory)
+        {
+            cpuState->Registers->HalfCarryFlag = true;
+            cpuState->Registers->SubtractionFlag = false;
+
+            //decode register and bit
+            auto bitIndex = (_opcode & 0x38) >> 3;
+            auto registerIndex = _opcode & 0x07;
+
+            cpuState->Registers->ZeroFlag = 
+                ((byte)(cpuState->Registers->ReadRegister(registerIndex) >> bitIndex) & 0x01) == 0;
+
+            Instruction::ExecuteCycle(cpuState, mainMemory);
+        }
+};
